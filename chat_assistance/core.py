10663e41a7fdecc7f5090f33c522569d9837e811
Rajat Nigam <rajatnigam89@gmail.com>
2023-11-27 00:52:33 +0530

    add memory to chat by replacing chain with conversationalRetrievalChain

--- chat_assistance/core.py
+++ chat_assistance/core.py
@@ -1,9 +1,9 @@
 import os
-from typing import Any
+from typing import Any, List, Dict
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models.openai import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import Pinecone
 import pinecone
 from constants import INDEX_NAME
@@ -14,19 +14,18 @@ pinecone.init(
 )
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
-        llm=chat,
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(),
-        return_source_documents=True,
+
+    ## Here we need memory to have context from previous prompts
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
 
 
 def root():
--- chat_assistance/gpt.py
+++ chat_assistance/gpt.py
@@ -12,6 +12,8 @@ if "user_prompt_history" not in st.session_state:
     st.session_state["user_prompt_history"] = []
 if "user_answer_history" not in st.session_state:
     st.session_state["user_answer_history"] = []
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
 
 def create_sources_string(source_urls: set[str]) -> str:
@@ -27,18 +29,22 @@ def create_sources_string(source_urls: set[str]) -> str:
 
 if prompt:
     with st.spinner("Generating Response..."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
         print(generated_response)
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
 
         formatted_response = (
-            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["user_answer_history"].append(formatted_response)
+        st.session_state["chat_history"].append((prompt, generated_response["answer"]))
+
 
 if st.session_state["user_answer_history"]:
     for generated_response, user_query in zip(
